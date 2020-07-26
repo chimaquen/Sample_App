6facced35a5454b1dec56b82967c6477a36ae469
Kenichi Shimada <tobeshimachan@yahoo.co.jp>
2020-07-26 16:53:14 +0000

    application.rd revised

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 
 module SampleApp
   class Application < Rails::Application
@@ -16,5 +16,7 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    config.assets.initialize_on_precompile = false
   end
 end
